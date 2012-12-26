7da13eee9ec8198faec83d3cec039a558a148fe2
Aaron Blakely <aaron@ephasic.org>
2012-12-26 09:58:04 -0500

    got some more work with ffmpeg.

--- app/routes/camera-stream.js
+++ app/routes/camera-stream.js
@@ -1,11 +1,11 @@
 module.exports = function(app, zeppelin) {
 	app.get('/camera-stream.png', function(req, res) {
-		//res.header({'Content-Type': 'image/png'});
-		//var pngStream = zeppelin.drone.createPngStream();
+		res.header({'Content-Type': 'image/png'});
+		var pngStream = zeppelin.drone.createPngStream();
 
-		//pngStream.on('data', function(data) {
-		//	console.log(data);
-		//	res.end(200, data);
-		//});
+		pngStream.on('data', function(data) {
+			console.log(data);
+			res.end(200, data);
+		});
 	});
 };
--- scripts/recieve-update.sh
+++ scripts/recieve-update.sh
@@ -8,6 +8,7 @@ USB_KEY="/data/video/usb"
 
 if [[ -d /zeppelin ]]; then
 	cp -ap "$USB_KEY/zeppelin" /
+	rm -rf /zeppelin/support
 else
 	if [[ -d /data/video/usb ]]; then
 		cp -ap /data/video/usb/zeppelin /data/video
@@ -16,4 +17,15 @@ else
 	mv -ap /data/video/zeppelin /zeppelin
 	cp /etc/init.d/rcS /etc/init.d/rcS~
 	echo '/zeppelin/bin/zeppelin' >> /etc/init.d/rcS
+fi
+
+# Conserve space on the root partition by
+# putting on the video partition.
+#
+if [[ !-d /data/zeppelin-support  ]]; then
+	if [[ -d /data/video/usb ]]; then
+		cp -ap /data/video/usb/zeppelin/support /data/zeppelin-support
+	else
+		cp -ap /data/video/zeppelin/support /data/zeppelin-support
+	fi
 fi
