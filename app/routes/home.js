0ab029c6f1e1e0fd8bf99e2b85262304dcca2297
Aaron Blakely <aaron@ephasic.org>
2012-12-27 20:13:51 -0500

    Added code for rendering battery percentage indicator

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,47 @@
+function displayBatteryPercent(percent) {
+	var maxChunk = 33;
+
+	var redColor    = 0;
+	var yellowColor = 0;
+	var greenColor  = 0;
+
+	for (var i = 0; i < percent; i++) {
+		if (redColor !== maxChunk) {
+			redColor++;
+		}
+
+		if (yellowColor !== maxChunk && redColor === maxChunk) {
+			yellowColor++;
+		}
+
+		if (greenColor !== maxChunk && yellowColor === maxChunk && redColor === maxChunk) {
+			greenColor++;
+		}
+	}
+
+	// each chunk is 33.3 when the percentage is 100.
+	if (redColor === maxChunk && yellowColor === maxChunk && greenColor === maxChunk) {
+		redColor	+= 0.3;
+		yellowColor	+= 0.3;
+		greenColor	+= 0.3;
+	}
+
+	return {
+		percent: percent,
+		red: redColor,
+		yellow: yellowColor,
+		green: greenColor
+	};
+}
+
 module.exports = function(app, zeppelin) {
 	app.get('/home', function(req, res) {
 		if (req.signedCookies['auth'] !== undefined) {
-			res.render('home');
+			var batStats = displayBatteryPercent(12);
+
+			res.render('home', {stats: {
+				battery: batStats
+			}});
 		} else {
 			res.redirect('/');
 		}
